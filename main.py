f1450871a0aa2286da52f5fd7977c2307e1c3d08
Snowy <140770098+snowystraw728@users.noreply.github.com>
2023-09-04 11:02:38 -0400

    Removed useless nextlink, explained what the code does in main.py

--- main.py
+++ main.py
@@ -14,38 +14,27 @@ def extract_player_xuids(data):
             if xuid:
                 player_xuids.append(xuid)
     return player_xuids
-
+#Input-------------------------------------------------
 club_id = input(f"{Fore.RED}Enter Club ID: {Fore.BLUE}")
 authorization_token = input(f"{Fore.RED}Enter Authorization Token: {Fore.BLUE}")
-
+#Headers-----------------------------------------------
 headers = {
     'x-xbl-contract-version': '2',
     'Authorization': authorization_token,
     'Accept-Language': 'en_us'
 }
-
+#Send Request-----------------------------------------
 base_url = f"https://clubhub.xboxlive.com/clubs/Ids({club_id})/decoration/clubpresence"
 response = requests.get(base_url, headers=headers)
-
+#Extract XUIDS----------------------------------------
 if response.status_code == 200:
     data = json.loads(response.text)
     player_xuids = extract_player_xuids(data)
-
-    next_link = data.get("nextLink")
-    while next_link:
-        response = requests.get(next_link, headers=headers)
-        if response.status_code == 200:
-            data = json.loads(response.text)
-            player_xuids += extract_player_xuids(data)
-            next_link = data.get("nextLink")
-        else:
-            print(f"Error: Status code {response.status_code}")
-            break
-
+#Write XUIDS-------------------------------------------
     with open(f"xuids.club.{club_id}.txt", "w", encoding="utf-8") as file:
         for xuid in player_xuids:
             file.write(xuid + "\n")
-
+#Print Response----------------------------------------
     print(f"{Fore.GREEN}XUIDs saved to xuids.club.{club_id}.txt{Style.RESET_ALL}")
 else:
     print(f"{Fore.RED} Error: Status code {response.status_code}")
